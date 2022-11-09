a67660be1c3eb94c8e8cf4fc224f45fbd7bf8ffe
qin yu peng <qin.batista@outlook.com>
2022-11-09 15:27:22 -0800

    update

--- GoogleDDNSClient.py
+++ GoogleDDNSClient.py
@@ -45,7 +45,7 @@ class GoogleDDNSClient:
             print("[my ip] "+self.__ip)
             if self.__ip != "":
                 _get_static_ip_stdout = open(self._fn_stdout, 'w+')
-                command = "proxychains curl -i -H 'Authorization:Basic "+self.__base64()+"' -H 'User-Agent: google-ddns-updater email@yourdomain.com' https://" + \
+                command = "curl -i -H 'Authorization:Basic "+self.__base64()+"' -H 'User-Agent: google-ddns-updater email@yourdomain.com' https://" + \
                     self.__username+":"+self.__password+"@domains.google.com/nic/update?hostname=" + \
                     self._my_domain+" -d 'myip="+self.__ip+"' > /dev/null"
                 print(command)
@@ -56,6 +56,7 @@ class GoogleDDNSClient:
                 time.sleep(10)
                 os.remove(self._fn_stdout)
         except Exception as e:
+            print("error when updating ip"+str(e))
             self.__log("error when updating ip"+str(e))
 
     def __get_host_ip(self):
