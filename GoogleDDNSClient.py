726844d5a6d04bde9ca82787bb5a7883697e5a1b
qin yu peng <qin.batista@outlook.com>
2022-11-09 16:05:53 -0800

    update

--- GoogleDDNSClient.py
+++ GoogleDDNSClient.py
@@ -7,20 +7,20 @@ import subprocess
 
 class GoogleDDNSClient:
     def __init__(self):
-        self._fn_stdout = "/root/ip_out"
-        self.__file_path = "/root/logs.txt"
-        self.__username = ""
-        self.__password = ""
-        self._my_domain = ""
-        with open("/Username.txt", "r") as f:
-            self.__username = f.readline()
-        with open("/Password.txt", "r") as f:
-            self.__password = f.readline()
-        with open("/domain_name.txt", "r") as f:
-            self._my_domain = f.readline()
-        print("self.__username:"+self.__username)
-        print("self.__password:"+self.__password)
-        print("self._my_domain:"+self._my_domain)
+        self._fn_stdout = "/Users/batista/Desktop/ip_out"
+        self.__file_path = "/Users/batista/Desktop/logs.txt"
+        self.__username = "IyLuDtYmPsnHMLtI"
+        self.__password = "6JrJzPTZDKwwi8j4"
+        self._my_domain = "ustest.qinyupeng.com"
+
+        # self._fn_stdout = "/root/ip_out"
+        # self.__file_path = "/root/logs.txt"
+        # with open("/Username.txt", "r") as f:
+        #     self.__username = f.readline()
+        # with open("/Password.txt", "r") as f:
+        #     self.__password = f.readline()
+        # with open("/domain_name.txt", "r") as f:
+        #     self._my_domain = f.readline()
         # https://domains.google.com/checkip banned by Chinese GFW
         self._get_ip_website = "https://checkip.amazonaws.com"
         self.__ip = ""
@@ -34,7 +34,7 @@ class GoogleDDNSClient:
         while True:
             try:
                 self.__post_ip_address()
-                time.sleep(600)
+                time.sleep(10)
             except Exception as e:
                 self.__log("_start"+str(e))
 
@@ -42,22 +42,17 @@ class GoogleDDNSClient:
         try:
             self.__ip = self.__get_host_ip()
             self.__log("[my ip] "+self.__ip)
-            print("[my ip] "+self.__ip)
             if self.__ip != "":
                 _get_static_ip_stdout = open(self._fn_stdout, 'w+')
                 command = "curl -i -H 'Authorization:Basic "+self.__base64()+"' -H 'User-Agent: google-ddns-updater email@yourdomain.com' https://" + \
                     self.__username+":"+self.__password+"@domains.google.com/nic/update?hostname=" + \
-                    self._my_domain+" -d 'myip="+self.__ip+"' > /dev/null"
+                    self._my_domain+" -d 'myip="+self.__ip+"'"
                 command = command.replace("\n", "")
                 process = subprocess.Popen(command, stdout=_get_static_ip_stdout,stderr=_get_static_ip_stdout, universal_newlines=True, shell=True)
                 process.wait()
-                with open(self._fn_stdout, "r") as f:
-                    result = f.read()
                 print(command)
-                print(result)
                 self.__log("[posted ip]"+str(self.__ip)+", wait for 10 seconds")
                 _get_static_ip_stdout.close()
-                time.sleep(10)
                 os.remove(self._fn_stdout)
         except Exception as e:
             print("error when updating ip"+str(e))
