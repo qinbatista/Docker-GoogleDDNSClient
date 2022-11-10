63829e2863a6c09204a339559aa49e0011867645
qin yu peng <qin.batista@outlook.com>
2022-11-09 16:27:37 -0800

    update

--- GoogleDDNSClient.py
+++ GoogleDDNSClient.py
@@ -5,16 +5,17 @@ import base64
 import threading
 import subprocess
 
+
 class GoogleDDNSClient:
     def __init__(self):
         self._fn_stdout = "/root/ip_out"
         self.__file_path = "/root/logs.txt"
         with open("/Username.txt", "r") as f:
-            self.__username = f.readline()
+            self.__username = f.readline().replace("\n", "")
         with open("/Password.txt", "r") as f:
-            self.__password = f.readline()
+            self.__password = f.readline().replace("\n", "")
         with open("/domain_name.txt", "r") as f:
-            self._my_domain = f.readline()
+            self._my_domain = f.readline().replace("\n", "")
         # https://domains.google.com/checkip banned by Chinese GFW
         self._get_ip_website = "https://checkip.amazonaws.com"
         self.__ip = ""
@@ -38,11 +39,10 @@ class GoogleDDNSClient:
             self.__log("[my ip] "+self.__ip)
             if self.__ip != "":
                 _get_static_ip_stdout = open(self._fn_stdout, 'w+')
-                command = "curl -i -H 'Authorization:Basic SXlMdUR0WW1Qc25ITUx0STo2SnJKelBUWkRLd3dpOGo0' -H 'User-Agent: google-ddns-updater email@yourdomain.com' https://" + \
+                command = "curl -i -H 'Authorization:Basic "+self.__base64() + "' -H 'User-Agent: google-ddns-updater email@yourdomain.com' https://" + \
                     self.__username+":"+self.__password+"@domains.google.com/nic/update?hostname=" + \
                     self._my_domain+" -d 'myip="+self.__ip+"'"
-                command = command.replace("\n", "")
-                process = subprocess.Popen(command, stdout=_get_static_ip_stdout,stderr=_get_static_ip_stdout, universal_newlines=True, shell=True)
+                process = subprocess.Popen(command, stdout=_get_static_ip_stdout, stderr=_get_static_ip_stdout, universal_newlines=True, shell=True)
                 process.wait()
                 self.__log("[posted ip]"+str(self.__ip)+", wait for 10 seconds")
                 _get_static_ip_stdout.close()
@@ -56,21 +56,15 @@ class GoogleDDNSClient:
             self.__ip = requests.get(self._get_ip_website).text.strip()
             return self.__ip
         except Exception as e:
-            print("[__get_host_ip ] "+str(e))
             self.__log("[__get_host_ip ] "+str(e))
             return ""
 
-
     def __log(self, result):
-        # if os.path.isfile(self.__file_path)==False:
-        # with open(self.__file_path,"a+") as f:pass
-        # print("key:key"+str(len(content)))
         with open(self.__file_path, "a+") as f:
             f.write(result+"\n")
         if os.path.getsize(self.__file_path) > 1024*128:
             with open(self.__file_path, "r") as f:
                 content = f.readlines()
-                # print("content:"+str(len(content)))
                 os.remove(self.__file_path)
 
     def __base64(self):
