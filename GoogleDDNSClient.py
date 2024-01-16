323153029eaa87f4c1e0d6ea4d7c38b7de05197d
qin.batista <qin.batista@outlook.com>
2024-01-16 00:02:18 -0800

    update

--- GoogleDDNSClient.py
+++ GoogleDDNSClient.py
@@ -27,7 +27,6 @@ class GoogleDDNSClient:
         return content
 
     def _start(self):
-        self.__log("_start")
         thread_refresh = threading.Thread(target=self._start_thread, name="t1", args=())
         thread_refresh.start()
 
@@ -45,25 +44,43 @@ class GoogleDDNSClient:
             this_ipv4 = self.__get_current_ipv4()
             self.__log(f"this_ipv6:{this_ipv6}")
             self.__log(f"this_ipv4:{this_ipv4}")
-            resultV4 = requests.post(f"https://{self.__google_username_v6}:{self.__google_password_v6}@domains.google.com/nic/update?hostname={self.__domain_name_v6}&myip={this_ipv6}")
+            if(this_ipv4!=""):resultV4 = requests.post(f"https://{self.__google_username_v6}:{self.__google_password_v6}@domains.google.com/nic/update?hostname={self.__domain_name_v6}&myip={this_ipv6}")
             self.__log(f"resultV4:{resultV4}")
-            resultV6 = requests.post(f"https://{self.__google_username_v4}:{self.__google_password_v4}@domains.google.com/nic/update?hostname={self.__domain_name_v4}&myip={this_ipv4}")
+            if(this_ipv6!=""):resultV6 = requests.post(f"https://{self.__google_username_v4}:{self.__google_password_v4}@domains.google.com/nic/update?hostname={self.__domain_name_v4}&myip={this_ipv4}")
             self.__log(f"resultV6:{resultV6}")
         except Exception as e:
             self.__log(f"[Error]_post_ip_address:{str(e)}")
 
     def __get_current_ipv6(self):
         try:
-            return requests.get(self._get_ipv6_website, timeout=5).text
-        except requests.exceptions.ConnectionError as ex:
-            return None
+            response = requests.get(self._get_ipv6_website, timeout=5)
+            response.raise_for_status()  # Raises an HTTPError for bad HTTP responses
+            return response.text.strip()
+        except requests.exceptions.HTTPError as errh:
+            self.__log(f"[get_host_ipv6] HTTP Error: {errh}")
+        except requests.exceptions.ConnectionError as errc:
+            self.__log(f"[get_host_ipv6] Error Connecting: {errc}")
+        except requests.exceptions.Timeout as errt:
+            self.__log(f"[get_host_ipv6] Timeout Error: {errt}")
+        except requests.exceptions.RequestException as err:
+            self.__log(f"[get_host_ipv6] Request Exception: {err}")
+        return None
 
     def __get_current_ipv4(self):
         try:
-            return requests.get(self._get_ipv4_website).text.strip()
-        except Exception as e:
-            self.__log("[get_host_ip] "+str(e))
-            return ""
+            response = requests.get(self._get_ipv4_website)
+            response.raise_for_status()  # Raises an HTTPError if the HTTP request returned an unsuccessful status code
+            return response.text.strip()
+        except requests.exceptions.HTTPError as errh:
+            self.__log(f"[get_host_ip] HTTP Error: {errh}")
+        except requests.exceptions.ConnectionError as errc:
+            self.__log(f"[get_host_ip] Error Connecting: {errc}")
+        except requests.exceptions.Timeout as errt:
+            self.__log(f"[get_host_ip] Timeout Error: {errt}")
+        except requests.exceptions.RequestException as err:
+            self.__log(f"[get_host_ip] Request Exception: {err}")
+        return ""
+
 
     def __log(self, result):
         with open(self.__file_path, "a+") as f:
