359409befc666eb87eba99558cc24c2c7e668642
qin.batista <qin.batista@outlook.com>
2024-01-16 14:05:34 -0800

    save

--- GoogleDDNSClient.py
+++ GoogleDDNSClient.py
@@ -44,11 +44,15 @@ class GoogleDDNSClient:
         try:
             this_ipv6 = self.__get_current_ipv6()
             this_ipv4 = self.__get_current_ipv4()
-            self.__log(f"this_ipv6:https://{self.__google_username_v6}:{self.__google_password_v6}@domains.google.com/nic/update?hostname={self.__domain_name_v6}&myip={this_ipv6}")
-            self.__log(f"this_ipv4:https://{self.__google_username_v4}:{self.__google_password_v4}@domains.google.com/nic/update?hostname={self.__domain_name_v4}&myip={this_ipv4}")
-            if(this_ipv4!=""):resultV4 = requests.post(f"https://{self.__google_username_v6}:{self.__google_password_v6}@domains.google.com/nic/update?hostname={self.__domain_name_v6}&myip={this_ipv6}",timeout=5)
+            ipv6_post_url = f"https://{self.__google_username_v6}:{self.__google_password_v6}@domains.google.com/nic/update?hostname={self.__domain_name_v6}&myip={this_ipv6}"
+            ipv6_post_url.replace("\n", "")
+            ipv4_post_url = f"https://{self.__google_username_v4}:{self.__google_password_v4}@domains.google.com/nic/update?hostname={self.__domain_name_v4}&myip={this_ipv4}"
+            ipv4_post_url.replace("\n", "")
+            self.__log(f"this_ipv6:{ipv6_post_url}")
+            self.__log(f"this_ipv4:{ipv4_post_url}")
+            if(this_ipv4!=""):resultV4 = requests.post(ipv6_post_url,timeout=5)
             self.__log(f"resultV4:{resultV4}")
-            if(this_ipv6!=""):resultV6 = requests.post(f"https://{self.__google_username_v4}:{self.__google_password_v4}@domains.google.com/nic/update?hostname={self.__domain_name_v4}&myip={this_ipv4}",timeout=5)
+            if(this_ipv6!=""):resultV6 = requests.post(ipv4_post_url,timeout=5)
             self.__log(f"resultV6:{resultV6}")
         except Exception as e:
             self.__log(f"[Error]_post_ip_address:{str(e)}")
